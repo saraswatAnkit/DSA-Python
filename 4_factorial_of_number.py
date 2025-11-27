8d9911674dc4f99b777055914af62b40029f2933
Ankit Saraswat <152775694+saraswatAnkit@users.noreply.github.com>
2025-11-27 18:58:01 +0530

    intro factorial of number

---
+++ 4_factorial_of_number.py
@@ -0,0 +1,10 @@
+#  factorial of a number
+
+def factorial(n):
+    if n==0 or n == 1:
+        return 1
+    return n * factorial(n-1)
+
+print(factorial(4))
+print(factorial(5))
+print(factorial(0))
